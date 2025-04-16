bafaf549fcb18ab3fd253a6a0c40869548b626d9
manylovv <yellowmanylov@gmail.com>
2025-04-16 13:52:50 +0500

    feat: pg

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,12 +2,9 @@ import type { Config } from "drizzle-kit";
 
 export default {
   out: "./drizzle",
-  schema: "./src/lib/server/schema/index.ts",
-  breakpoints: true,
-  verbose: true,
+  schema: "./src/lib/db/schema.ts",
   strict: true,
   dialect: "postgresql",
-  casing: "snake_case",
   dbCredentials: {
     url: process.env.DATABASE_URL as string,
   },
